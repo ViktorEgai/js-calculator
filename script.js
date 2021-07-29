0b52a1f229822cc3a63d9130d8c788769132e3ec
Egai Viktor <egai.viktor94@gmail.com>
2021-07-29 13:50:09 +0300

    Создал переменные и вывел сообщения в консоль

---
+++ script.js
@@ -0,0 +1,9 @@
+let money = 100;
+let income = 1200;
+let addExpenses = 20;
+let deposit = 130;
+let mission = '';
+let period = '';
+
+alert('Привет Мир! ');
+console.log(' Создал первые переменные в JS');
