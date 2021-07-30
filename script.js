d74d9ba4969097733dd2ee7846289335f523f43e
Egai Viktor <egai.viktor94@gmail.com>
2021-07-30 15:38:48 +0300

    Вывел в консоль все переменные

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 let money = 25000,
   income = 'пассивный доход',
-  addExpenses = 'Коммунальные услугы, бензин, домашний интернет, курсы',
+  addExpenses = 'Коммунальные услуги, бензин, домашний интернет, курсы',
   deposit = true,
   mission = 150000,
-  period = 6;
+  period = 6, 
+  budgetDay = money / 30;
+
+  console.log('Тип данных переменной money: ' + typeof money);
+  console.log('Тип данных переменной income: ' + typeof income);
+  console.log('Тип данных переменной deposit: ' + typeof deposit);
+  console.log('Длина строки addExpenses: ' + addExpenses.length);
+  console.log('Период равен '+ period +' месяцев');
+  console.log('Цель заработать ' + mission + ' рублей');
+  console.log(addExpenses.toLowerCase().split(', '));
+  console.log('Дневной бюджет составляет ' + budgetDay + ' руб.');
