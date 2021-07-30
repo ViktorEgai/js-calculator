56d9822ad16a9d3d5b464e9bcc0e6c570bf7a05d
Egai Viktor <egai.viktor94@gmail.com>
2021-07-30 15:19:46 +0300

    Изменил значение переменных

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-let money = 100;
-let income = 1200;
-let addExpenses = 20;
-let deposit = 130;
-let mission = '';
-let period = '';
-
-alert('Привет Мир! ');
-console.log(' Создал первые переменные в JS');
+let money = 25000,
+  income = 'пассивный доход',
+  addExpenses = 'Коммунальные услугы, бензин, домашний интернет, курсы',
+  deposit = true,
+  mission = 150000,
+  period = 6;
