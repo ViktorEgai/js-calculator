0e7f482a1a93430b3b8ded5f58957e725da55702
Egai Viktor <egai.viktor94@gmail.com>
2021-08-02 14:18:52 +0300

    Написал условие по уровню дохода и изменил перменные

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
-let money = 25000,
+'use strict';
+
+let money = prompt('Ваш месячный доход'),
   income = 'пассивный доход',
-  addExpenses = 'Коммунальные услуги, бензин, домашний интернет, курсы',
-  deposit = true,
-  mission = 150000,
-  period = 6, 
-  budgetDay = money / 30;
+  addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+  deposit = confirm('Есть ли у вас депозит в банке?'),
+  mission = 1500000,  
+  expenses1 = prompt('Введите обязательную статью расходов?'),
+  expenses2 = prompt('Введите обязательную статью расходов?'),
+  amount1 = prompt('Во сколько это обойдется?'),
+  amount2 = prompt('Во сколько это обойдется?'),
+  budgetMonth = +money - (+amount1 + +amount2),
+  period = mission / budgetMonth,
+  budgetDay =  budgetMonth / 30;  
 
   console.log('Тип данных переменной money: ' + typeof money);
   console.log('Тип данных переменной income: ' + typeof income);
@@ -14,3 +21,18 @@ let money = 25000,
   console.log('Цель заработать ' + mission + ' рублей');
   console.log(addExpenses.toLowerCase().split(', '));
   console.log('Дневной бюджет составляет ' + budgetDay + ' руб.');
+  console.log('budgetMonth: ', budgetMonth);
+  console.log('period: ', Math.ceil(period));
+  console.log('budgetDay: ', Math.floor(budgetDay));
+  
+  if (budgetDay >= 1200) {
+    console.log('У вас высокий уровень дохода');
+  } else if (budgetDay < 1200 && budgetDay >= 600) {
+    console.log('У вас средний уровень дохода');
+  } else if (budgetDay < 600 && budgetDay > 0) {
+    console.log('К сожалению у вас уровень дохода ниже среднего');
+  } else {
+    console.log('Что то пошло не так');
+  }
+
+ 
