a90cba80d0a58d3d02842ebed47ec90dfa1d7a91
Egai Viktor <egai.viktor94@gmail.com>
2021-08-09 13:15:31 +0300

    Присвоил переменные элементам страницы

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 'use strict';
+
+const countButton = document.getElementById('start');
+const incomeAddButton = document.getElementsByTagName('button')[0];
+const expenseesAddButton = document.getElementsByTagName('button')[1];
+const depositCheckbox = document.querySelector('#deposit-check');
+const additionIncomeInput = document.querySelectorAll('.additional_income-item');
+const budgetMonthValue = document.getElementsByClassName('budget_month-value');
+const budgetDayValue = document.getElementsByClassName('budget_day-value');
+const expensesMonthValue = document.getElementsByClassName('expenses_month-value');
+const additionalIncomeValue = document.getElementsByClassName('additional_income-value');
+const additionalExpensesValue = document.getElementsByClassName('additional_expenses-value');
+const incomePeriodValue = document.getElementsByClassName('income_period-value');
+const targetMonthValue = document.getElementsByClassName('target_month-value');
+const salaryAmount = document.querySelector('.salary-amount');
+const incomeTitle = document.querySelector('.income-title');
+const incomeAmount = document.querySelector('.income-amount');
+const expensesTitle = document.querySelector('.expenses-title');
+const expensesAmount = document.querySelector('.expenses-amount');
+const additionalExpensesItem = document.querySelector('.additional_expenses-item');
+const depositAmount = document.querySelector('.deposit-amount');
+const depositPercent = document.querySelector('.deposit-percent');
+const periodSelect = document.querySelector('.period-select');
 const isNumber = function(n) {
   return !isNaN(parseFloat(n)) && isFinite(n);
 }
@@ -169,7 +191,4 @@ for (let key in appData) {
 
 
 console.log(appData.addExpenses.join(', '));
-
-
-
 
